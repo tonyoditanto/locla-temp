2c9a8e6bc387d8a4b7b65c14cddd59820f8b3d11
Rizal Hidayat <realrizalhidayat@gmail.com>
2020-08-04 09:54:19 +0700

    Initial Commit

---
+++ Locla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Locla
+//
+//  Created by Rizal Hidayat on 04/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
