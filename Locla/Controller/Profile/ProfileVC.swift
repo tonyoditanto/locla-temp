0fa2f396431ca2b7302daa286fa91fcd3dfcef76
edwarddacosta <62272350+edwarddacosta@users.noreply.github.com>
2020-08-10 08:42:18 +0700

    Pembuatan Profile dan Asset profile

--- Locla/Controller/Profile/ProfileVC.swift
+++ Locla/Controller/Profile/ProfileVC.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       // backProfil = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        backProfil.image = UIImage(named : "backgroundProfil")
+        ProfilPic.image = UIImage(named: "people2")
+        bintang.image = UIImage(named: "star")
+        percobaanPertama.image = UIImage(named: "Image-1")
+        bgBintang.layer.cornerRadius = 10
+        bgPercobaanPertama.layer.cornerRadius = 10
+        bgBintang.backgroundColor = UIColor.init(red: 242/255, green: 242/255, blue: 242/255, alpha: 100)
+        bgPercobaanPertama.backgroundColor = UIColor.init(red: 242/255, green: 242/255, blue: 242/255, alpha: 100)
+        imgHadiah.image = UIImage(named: "reward")
+        bgHadiah.backgroundColor = UIColor.init(red: 250/255, green: 120/255, blue: 0/255, alpha: 100)
+        bgHadiah.layer.cornerRadius = 10
+        //ProfilPic.layer.cornerRadius = 14
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var bgHadiah: UIImageView!
+    @IBOutlet weak var imgHadiah: UIImageView!
+    @IBOutlet weak var bgPercobaanPertama: UIImageView!
+    @IBOutlet weak var bintang: UIImageView!
+    @IBOutlet weak var percobaanPertama: UIImageView!
+    @IBOutlet weak var backProfil: UIImageView!
+    @IBOutlet weak var ProfilPic: UIImageView!
+    @IBOutlet weak var bgBintang: UIImageView!
     /*
     // MARK: - Navigation
 
