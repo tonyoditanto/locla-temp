792d79c19aef4f5da0b5eb4f6b37f0fda1d58705
Bagus setiawan <setiawanbagus4315@gmail.com>
2020-08-10 19:23:09 +0700

    add searchBar

--- Locla/Controller/Vocabulary/VocabularyVC.swift
+++ Locla/Controller/Vocabulary/VocabularyVC.swift
@@ -13,8 +13,8 @@ class VocabularyVC: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var searcBar: UISearchBar!
     @IBOutlet weak var categoryVocabCollectionView: UICollectionView!
     
-    //var dataCategoryVocabs : [categoryVocab] = []
-    var colors : [UIColor] = [#colorLiteral(red: 0.9294117647, green: 0.6235294118, blue: 0.2235294118, alpha: 1),#colorLiteral(red: 0.3098039216, green: 0.4196078431, blue: 0.862745098, alpha: 1),#colorLiteral(red: 0.5215686275, green: 0.368627451, blue: 0.8470588235, alpha: 1),#colorLiteral(red: 0.9803921569, green: 0.3921568627, blue: 0, alpha: 1),#colorLiteral(red: 0.5568627451, green: 0.7490196078, blue: 0.4039215686, alpha: 1),#colorLiteral(red: 0.4901960784, green: 0.1411764706, blue: 0.2274509804, alpha: 1)]
+    var colors : [UIColor] = [#colorLiteral(red: 0.9294117647, green: 0.6235294118, blue: 0.2235294118, alpha: 1),#colorLiteral(red: 0.3098039216, green: 0.4196078431, blue: 0.862745098, alpha: 1),#colorLiteral(red: 0.5215686275, green: 0.368627451, blue: 0.8470588235, alpha: 1),#colorLiteral(red: 0.9803921569, green: 0.3921568627, blue: 0, alpha: 1),#colorLiteral(red: 0.5568627451, green: 0.7490196078, blue: 0.4039215686, alpha: 1),#colorLiteral(red: 0.4901960784, green: 0.1411764706, blue: 0.2274509804, alpha: 1),#colorLiteral(red: 0.4901960784, green: 0.1411764706, blue: 0.2274509804, alpha: 1),#colorLiteral(red: 0.4901960784, green: 0.1411764706, blue: 0.2274509804, alpha: 1),#colorLiteral(red: 0.4901960784, green: 0.1411764706, blue: 0.2274509804, alpha: 1),#colorLiteral(red: 0.4901960784, green: 0.1411764706, blue: 0.2274509804, alpha: 1)]
+    let searchController = UISearchController(searchResultsController: nil)
     let MyCollectionViewCellId: String = "vocabularyCategoryCell"
     
     override func viewDidLoad() {
@@ -22,13 +22,20 @@ class VocabularyVC: UIViewController, UISearchBarDelegate {
 
         // Do any additional setup after loading the view.
         
-        self.categoryVocabCollectionView.delegate = self
-        self.categoryVocabCollectionView.dataSource = self
-        
         let nibCell = UINib(nibName: MyCollectionViewCellId, bundle: nil)
         categoryVocabCollectionView.register(nibCell,forCellWithReuseIdentifier: MyCollectionViewCellId)
         
+        setupSearchController()
+    }
+    
+    func setupSearchController() {
         
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        definesPresentationContext = true
+
+
     }
 }
 
@@ -59,10 +66,6 @@ extension VocabularyVC: UICollectionViewDelegate, UICollectionViewDataSource, UI
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCellId, for: indexPath) as! vocabularyCategoryCell
         cell.backgroundColor = colors[indexPath.row]
      
-        
-        //cell.productImage.layer.cornerRadius = cell.productImage.frame.height / 15
-        
-        
         return cell
     }
 }
