03c5995f35983429fb49089822c0dfb58dc8898b
Tony Varian Yoditanto <tonyoditanto@gmail.com>
2020-08-10 11:40:15 +0700

    Add Storyboard : Topik & Subtopik

---
+++ Locla/View/Lesson/Subtopik/cells/SubtopikCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SubtopikCollectionViewCell.swift
+//  Locla
+//
+//  Created by Tony Varian Yoditanto on 10/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class SubtopikCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Locla/View/Lesson/Topik/cells/TopikCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TopikCollectionViewCell.swift
+//  Locla
+//
+//  Created by Tony Varian Yoditanto on 10/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class TopikCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
