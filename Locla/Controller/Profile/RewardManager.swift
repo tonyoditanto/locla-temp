b2a80ecf5dbd3f02493ba10e9039b5f7a64dd44a
edwarddacosta <62272350+edwarddacosta@users.noreply.github.com>
2020-08-11 04:01:56 +0700

    profil plus reward

--- Locla/Controller/Profile/RewardManager.swift
+++ Locla/Controller/Profile/RewardManager.swift
@@ -13,7 +13,7 @@ public class RewardManager {
     var arrayOfReward: [Reward]
    init() {
     arrayOfReward = [Reward(name: "KenjeranPark", lokasi: "Jalan A", imageRewardName: "kenjeranpark.jpg", descReward: "tempat hiburan untuk keluarga dan teman, dengan berbagai macam wahana yang sangat menghibur dan juga berbagai tempat foto yang sangat instagramable"),
-    Reward(name: "KenjeranPark2", lokasi: "Jalan b", imageRewardName: "", descReward: "tempat hiburan2"),
-    Reward(name: "KenjeranPark3", lokasi: "Jalan C", imageRewardName: "", descReward: "tempat hiburan3")]
+    Reward(name: "Manggrove", lokasi: "Jalan b", imageRewardName: "", descReward: "tempat hiburan2"),
+    Reward(name: "Surabaya Carnival", lokasi: "Jalan C", imageRewardName: "", descReward: "tempat hiburan3")]
     }
 }
