8e7342fdc13f72d1b53be6a9071bdfcaab71cc3d
Rizal Hidayat <realrizalhidayat@gmail.com>
2020-08-11 14:34:12 +0700

    bug fixed

--- Locla/Controller/Lesson/LessonVC.swift
+++ Locla/Controller/Lesson/LessonVC.swift
@@ -2,61 +2,29 @@
 //  LessonVC.swift
 //  Locla
 //
-//  Created by Rizal Hidayat on 04/08/20.
+//  Created by Rizal Hidayat on 11/08/20.
 //  Copyright © 2020 Apple Developer Academy. All rights reserved.
 //
 
 import UIKit
 
-class LessonVC: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource {
+class LessonVC: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchTopiks()
-        setupCollectionView()
-    }
-    
-    func fetchTopiks(){
-    }
-    
-    func setupCollectionView() {
-        registerCollectionView()
-    }
-    
-    func registerCollectionView(){
-        let nib = UINib(nibName: TopikCollectionViewCell.cellID, bundle: Bundle.main)
-        self.collectionView.register(nib, forCellWithReuseIdentifier: TopikCollectionViewCell.cellID)
-    }
-    
-// MARK: UICollectionViewDataSource
 
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        // Do any additional setup after loading the view.
     }
-        
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
-    }
-        
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TopikCollectionViewCell.cellID, for: indexPath) as! TopikCollectionViewCell
-        return cell
-    }
-        
-        
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-        performSegue(withIdentifier: "showSubtopik", sender: indexPath)
-    }
+    
 
-        
-// MARK: - Sent Data to other ViewController
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//
-//    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
-
