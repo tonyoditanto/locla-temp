f2536e58f3220fc216ff8b5ceaa9bacd914599a3
Rizal Hidayat <realrizalhidayat@gmail.com>
2020-08-12 08:28:23 +0700

    RewardCategory added

--- Locla/Model/VocabularyCategory.swift
+++ Locla/Model/VocabularyCategory.swift
@@ -11,12 +11,13 @@ import Foundation
 // MARK: - VocabularyCategoryElement
 struct VocabularyCategory: Codable {
     let id, locationID: Int
-    let name, imageFilename: String?
+    let name, imageFilename, hexColor: String?
 
     enum CodingKeys: String, CodingKey {
         case id
         case locationID = "location_id"
         case name
         case imageFilename = "image_filename"
+        case hexColor = "hex_color"
     }
 }
