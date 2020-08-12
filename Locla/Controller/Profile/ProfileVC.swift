134612761dca790d42aa5b3d62d47cac1f8cf4a2
edwarddacosta <62272350+edwarddacosta@users.noreply.github.com>
2020-08-12 09:22:43 +0700

    implement tasi fitur rename dan tracking progressview

--- Locla/Controller/Profile/ProfileVC.swift
+++ Locla/Controller/Profile/ProfileVC.swift
@@ -7,13 +7,62 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ProfileVC: UIViewController {
 
     @IBOutlet weak var buttonHadiah: UIButton!
     
+       @IBAction func buttonHadiah(_ sender: Any) {
+       }
+       @IBOutlet weak var progressView: UIProgressView!
+       @IBOutlet weak var imgHadiah: UIImageView!
+       @IBOutlet weak var bgPercobaanPertama: UIImageView!
+       @IBOutlet weak var bintang: UIImageView!
+       @IBOutlet weak var percobaanPertama: UIImageView!
+       @IBOutlet weak var backProfil: UIImageView!
+       @IBOutlet weak var ProfilPic: UIImageView!
+       @IBOutlet weak var bgBintang: UIImageView!
+    
+    @IBOutlet weak var lblnama: UILabel!
+    @IBAction func renameButton(_ sender: Any) {
+        // create the actual alert controller view that will be the pop-up
+        let alertController = UIAlertController(title: "Welcome", message: "Input your Name", preferredStyle: .alert)
+
+        alertController.addTextField { (textField) in
+            // configure the properties of the text field
+            textField.placeholder = "Name"
+        }
+
+
+        // add the buttons/actions to the view controller
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+
+            // this code runs when the user hits the "save" button
+
+            let inputName = alertController.textFields![0].text
+
+            print(inputName)
+            self.lblnama.text = inputName
+
+        }
+
+        alertController.addAction(cancelAction)
+        alertController.addAction(saveAction)
+
+        present(alertController, animated: true, completion: nil)
+       
+    }
+    @IBOutlet weak var lblPV: UILabel!
+    var status : Status?
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ProgressLearning()
        // backProfil = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
       //  backProfil.image = UIImage(named : "backgroundProfil")
        // ProfilPic.image = UIImage(named: "people2")
@@ -30,27 +79,35 @@ class ProfileVC: UIViewController {
         progressView.clipsToBounds = true
         
         //ProfilPic.layer.cornerRadius = 14
-        // Do any additional setup after loading the view.
+       
+    
+    }
+    var maxTime : Float = 10.0
+        var currentTime : Float = -1.1
+    
+    @objc func updateProgress(){
+        currentTime = currentTime + 1.1
+           progressView.progress = currentTime/maxTime
+    
+       }
+       
+       @objc func decreaseProgress (){
+           currentTime = currentTime - 1.1
+           progressView.progress = currentTime/maxTime
+       }
+    func ProgressLearning() {
+        if status == Status(rawValue: "Unlocked"){
+            perform(#selector(updateProgress))
+            self.lblPV.text = "\(Int(self.progressView.progress * 100))%"
+        }
+        else {
+            perform(#selector(decreaseProgress))
+        }
+        
     }
     
-    @IBAction func buttonHadiah(_ sender: Any) {
-    }
-    @IBOutlet weak var progressView: UIProgressView!
-    @IBOutlet weak var imgHadiah: UIImageView!
-    @IBOutlet weak var bgPercobaanPertama: UIImageView!
-    @IBOutlet weak var bintang: UIImageView!
-    @IBOutlet weak var percobaanPertama: UIImageView!
-    @IBOutlet weak var backProfil: UIImageView!
-    @IBOutlet weak var ProfilPic: UIImageView!
-    @IBOutlet weak var bgBintang: UIImageView!
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
+    
 }
