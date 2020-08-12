0ba5d7230e222109c12dc42de6422f8cb9c5af5a
Bagus setiawan <setiawanbagus4315@gmail.com>
2020-08-12 20:29:47 +0700

    add searchBar result view !

---
+++ Locla/Controller/Vocabulary/SearchBarVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchBarVC.swift
+//  Locla
+//
+//  Created by Bagus setiawan on 12/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
