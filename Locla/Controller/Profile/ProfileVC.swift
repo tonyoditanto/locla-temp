61042851a16c63d0b610dd03991fdc5059869b9e
Rizal Hidayat <realrizalhidayat@gmail.com>
2020-08-14 00:12:21 +0700

    profile page fixed

--- Locla/Controller/Profile/ProfileVC.swift
+++ Locla/Controller/Profile/ProfileVC.swift
@@ -11,20 +11,15 @@ import Foundation
 
 
 class ProfileVC: UIViewController {
-
-    @IBOutlet weak var hadiahBtn: UIButton!
-       @IBOutlet weak var progressView: UIProgressView!
-       @IBOutlet weak var imgHadiah: UIImageView!
-       @IBOutlet weak var bgPercobaanPertama: UIImageView!
-       @IBOutlet weak var bintang: UIImageView!
-       @IBOutlet weak var percobaanPertama: UIImageView!
-       @IBOutlet weak var backProfil: UIImageView!
-       @IBOutlet weak var ProfilPic: UIImageView!
-       @IBOutlet weak var bgBintang: UIImageView!
-    @IBOutlet weak var lblBintang: UILabel!
-    @IBOutlet weak var lblPercobaanPertama: UILabel!
-    @IBOutlet weak var lblnama: UILabel!
-    @IBOutlet weak var lblPV: UILabel!
+    
+    @IBOutlet weak var userPV: UIProgressView!
+    @IBOutlet weak var profilIV: UIImageView!
+    @IBOutlet weak var bintang: UIImageView!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var totalStarLbl: UILabel!
+    @IBOutlet weak var firstTryLbl: UILabel!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var percentageLbl: UILabel!
     
     var totalPercobaanPertama : Int?
     var totalstar : Int?
@@ -34,104 +29,80 @@ class ProfileVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        progressLearning()
-        fetchTopics()
-        bgBintang.layer.cornerRadius = 10
-        bgPercobaanPertama.layer.cornerRadius = 10
-        bgBintang.backgroundColor = UIColor.init(red: 242/255, green: 242/255, blue: 242/255, alpha: 100)
-        bgPercobaanPertama.backgroundColor = UIColor.init(red: 242/255, green: 242/255, blue: 242/255, alpha: 100)
-        hadiahBtn.backgroundColor = UIColor.init(red: 250/255, green: 120/255, blue: 0/255, alpha: 100)
-        hadiahBtn.layer.cornerRadius = 10
-        progressView.layer.cornerRadius = 10
-        progressView.clipsToBounds = true
-        getStar()
-        //ProfilPic.layer.cornerRadius = 14
+        setupView()
+         fetchTopics()
+        setProgressLearning()
        
+        getStar()
+    }
     
+    func setupView(){
+        profilIV.layer.borderWidth = 4.0
+        profilIV.layer.borderColor = #colorLiteral(red: 0.9818729758, green: 0.6061103344, blue: 0.01341408864, alpha: 1)
+        profilIV.layer.cornerRadius = profilIV.frame.size.width / 2
     }
     
     func fetchTopics(){
         topics = DataLoader.getTopics(locationID: locationId ?? 1)
-       }
+    }
     
     func getStar (){
-        
         var subtopics : [Subtopic] = []
         for topic in topics {
             subtopics.append(contentsOf: DataLoader.getSubtopics(topicID: topic.id))
         }
-       var stargained = 0
-              for subtopic in subtopics {
-                stargained += subtopic.starGained
-              }
+        var stargained = 0
+        for subtopic in subtopics {
+            stargained += subtopic.starGained
+        }
         if stargained == 0 {
-            lblBintang.text = "0"
-           print (stargained)
+            totalStarLbl.text = "0"
+            print (stargained)
         }
         else {
-             lblBintang.text = "\(stargained)"
+            totalStarLbl.text = "\(stargained)"
         }
-       
+        
     }
-   
+    
     func getPercobaanPertama () {
-       
-    }
         
-    
-    //rename
-       @IBAction func renameButton(_ sender: Any) {
-          //pop up alert rename
-           let alertController = UIAlertController(title: "Welcome", message: "Input your Name", preferredStyle: .alert)
-
-           alertController.addTextField { (textField) in
-               // configure the properties of the text field
-               textField.placeholder = "Name"
-           }
-
-
-           // add the buttons/actions to the view controller
-           let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-           let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
-
-
-               let inputName = alertController.textFields![0].text
-
-               //print(inputName)
-               self.lblnama.text = inputName
-           }
-           alertController.addAction(cancelAction)
-           alertController.addAction(saveAction)
-
-           present(alertController, animated: true, completion: nil)
-          
-       }
-    @IBAction func buttonHadiah(_ sender: Any) {
     }
     
-
-    var maxTime : Float = 10.0
-    var currentTime : Float = 0
-    
-    @objc func updateProgress(){
-        currentTime = currentTime + 1.1
-           progressView.progress = currentTime/maxTime
-       }
-       
-       @objc func decreaseProgress (){
-           currentTime = currentTime - 1.1
-           progressView.progress = currentTime/maxTime
-       }
-    func progressLearning() {
-     
+    @IBAction func onRenameBtnClicked(_ sender: UIButton) {
+        //pop up alert rename
+        let alertController = UIAlertController(title: "Change name", message: "Input your name", preferredStyle: .alert)
         
-        if Status.unlocked.rawValue == "Unlocked" {
-            perform(#selector(updateProgress))
-            self.lblPV.text = "\(Int(self.progressView.progress * 100))%"
+        alertController.addTextField { (textField) in
+            // configure the properties of the text field
+            textField.placeholder = "Name"
         }
-        else {
-            perform(#selector(decreaseProgress))
+        
+        
+        // add the buttons/actions to the view controller
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            let inputName = alertController.textFields![0].text
+            self.nameLbl.text = inputName
         }
+        alertController.addAction(cancelAction)
+        alertController.addAction(saveAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    @IBAction func onRewardBtnClicked(_ sender: Any) {
+    }
+    
+    func setProgressLearning() {
+        var subtopics : [Subtopic] = []
+        for topic in topics {
+            subtopics.append(contentsOf: DataLoader.getSubtopics(topicID: topic.id))
+        }
+        let unlockedSubtopics = subtopics.filter{ $0.status == Status.cleared}
+        userPV.progress = Float(unlockedSubtopics.count) / Float(subtopics.count)
+        print(userPV.progress)
+        percentageLbl.text = "\(Int((userPV.progress * 100).rounded())) % "
         
     }
     
