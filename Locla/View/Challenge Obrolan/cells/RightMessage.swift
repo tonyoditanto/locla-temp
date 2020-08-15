247a3c81a571b083bf5269bba61762f4d62314a6
Bagus setiawan <setiawanbagus4315@gmail.com>
2020-08-15 22:35:40 +0700

    add xib file message !

---
+++ Locla/View/Challenge Obrolan/cells/RightMessage.swift
@@ -0,0 +1,24 @@
+//
+//  RightMessage.swift
+//  Locla
+//
+//  Created by Bagus setiawan on 15/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class RightMessage: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
